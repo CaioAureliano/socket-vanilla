18139084a7b77b9bd4da589efdbc7b5451f9f12b
Caio Aureliano <caio.aureliano1@gmail.com>
2023-02-04 17:40:02 -0300

    refactor: server

--- server/src/server.js
+++ server/src/server.js
@@ -1,31 +1,13 @@
-const http = require('http');
-const ws = require('ws');
+const { WebSocketServer } = require('ws');
 
-const wss = new ws.Server({ noServer: true });
+const wss = new WebSocketServer({ port: 8080 });
 
-const accept = (req, res) => {
-    if (!req.headers.upgrade || req.headers.upgrade.toLowerCase() != 'websocket') {
-        res.end();
-        return;
-    }
+wss.on('connection', (ws) => {
+    ws.on('error', console.error);
 
-    if (!req.headers.connection.match(/\bupgrade\b/i)) {
-        res.end();
-        return;
-    }
-
-    wss.handleUpgrade(req, req.socket, Buffer.alloc(0), onConnect);
-};
-
-const onConnect = (ws) => {
-    ws.on('message', (message) => {
-        console.log(message);
-        ws.send(`Hello!`);
-
-        setTimeout(() => ws.close(1000, "Bye!"), 5000);
+    ws.on('message', (data) => {
+        console.log(data);
     });
 
-    ws.on('error', (error) => console.error(error));
-};
-
-http.createServer(accept).listen(3000);
+    ws.send('Hello!');
+});
